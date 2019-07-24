9ca264fe20bb5ccb69c8e94b677f1c427434053a
arthurharrison <tavares593@gmail.com>
2019-07-24 03:22:34 -0300

    Fixed bugs in the function main and in the counter aswell, and added new Extensions

--- organizer.py
+++ organizer.py
@@ -1,29 +1,34 @@
 import os
 import shutil
 
-# por fim adicionar interface visual para nao precisar por o script no local, e pela interface informa a pasta, e assim pego o path
+# TODO: Better documentation, do a GUI, deal with a proper count of the files located
 PATH = './'
 EXTENSIONS = {
     "EXECUTABLES": ['.exe', '.msi', '.apk'],
     "IMAGES": ['.jpeg', '.jpg', '.tiff', '.gif', '.bmp',
-               '.png', '.PNG', '.bpg', '.svg', '.heif', '.psd'],
+               '.png', '.bpg', '.svg', '.heif', '.psd'],
     "VIDEOS": [".avi", ".flv", ".wmv", ".mov", ".mp4", ".webm",
-               ".mpg", ".mpeg", ".3gp"],
+               ".vob", ".mng", ".qt", ".mpg", ".mpeg", ".3gp", ".mkv"],
     "AUDIO": [".aac", ".aa", ".aac", ".dvf", ".m4a", ".m4b", ".m4p", ".mp3",
               ".msv", "ogg", "oga", ".raw", ".vox", ".wav", ".wma"],
-    "DOCUMENTS": ['.txt',  '.docx', '.doc', '.fdf', '.ods',
+    "DOCUMENTS": ['.epub', '.pages', '.docx', '.doc', '.fdf', '.ods',
                   '.odt', '.xps', '.dotx', '.docm', '.dox', '.xls', '.xlsx', '.ppt', '.pptx'],
+    "PLAINTEXT": [".txt", ".in", ".out"],
     "PDFS": ['.pdf'],
-    "ZIPPEDS": ['.rar', '.zip', '.7z',
-                '.gzip']
-
+    "ZIPPEDS": ['.rar', '.zip', '.7z', '.bzip2',
+                '.gzip', '.tar', '.wim', '.xz', ]
 }
 
 
 def printFilesList(files):
+    fileListTotal = 0
     print("The following files are located in this folder:")
     for file in files:
+        if('.' in file):
+            fileListTotal += 1
         print(file)
+    fileListTotal -= 1  # excluding the .py file that executes this
+    return fileListTotal
 
 
 def organizer(fileName, destinyFolder):
@@ -42,19 +47,19 @@ def filesMover(fileName, destinyFolder):
 
 def main():
     filesList = os.listdir(PATH)
-    printFilesList(filesList)
+    fileListTotal = printFilesList(filesList)
 
-    confirm = input("Should you proceed with this operation ?(Y/N)")
+    confirm = input("\nShould you proceed with this operation ?(Y/N)\n")
     if(confirm[0].capitalize() == "N"):
         print("Have a good day :)\nBye!")
         exit()
 
     filesListIndex = 1
-    fileListTotal = len(filesList)
+
     for currentFile in filesList:
         currentFileExtension = str(currentFile[currentFile.rfind('.'):])
 
-        for fType, extList in EXTENSIONS:
+        for fType, extList in EXTENSIONS.items():
             for currentExtension in extList:
                 if currentFileExtension == currentExtension:
                     print(
